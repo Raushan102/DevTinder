8bc1f961f9bae42d31e81a30a7b1fb58adee4352
Raushan kumar saw <124686068+Raushan102@users.noreply.github.com>
2025-12-30 13:50:38 +0530

    a bug finxed

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const url =
-  "mongodb+srv://raushankumarsaw952_db_user:Raushan%402003@cluster0.idrdatc.mongodb.net/devTinder?appName=Cluster0";
+const url = process.env.MONGODB_URI;
 
 async function connect() {
   await mongoose.connect(url);
